2a36d94100f37fbf57bf8d45b9c4adc08ab3dc44
Abu Zer Arshad <aarshad.bese15seecs@seecs.edu.pk>
2020-08-06 21:13:09 +0500

    LSTM on sweden dataset

---
+++ Models/swedenLSTM.py
@@ -0,0 +1,2 @@
+# load all data
+dataset = read_csv('household_power_consumption.txt', sep=';', header=0, low_memory=False, infer_datetime_format=True, parse_dates={'datetime':[0,1]}, index_col=['datetime'])
